e4ef2e0530b3c51809aab2c2c63b5e1f31852a41
Andrey Cespedes <andrey42002@gmail.com>
2025-07-24 16:52:04 -0500

    Arreglo de imagenes en el main

---
+++ src/components/LugaresInfo.js
@@ -0,0 +1,37 @@
+const lugares = [
+    {
+        src: "/images/image1_Asistencia.jpeg",
+        alt: "Asistencia con el Entrenador",
+        titulo: "Asistencia con el Entrenador"
+    },
+    {
+        src: "/images/image2_Zona_Cardio.jpeg",
+        alt: "Zona de Cardio",
+        titulo: "Zona de Cardio"
+    },
+    {
+        src: "/images/image3_Reglamento.jpeg",
+        alt: "Zona de Máquinas",
+        titulo: "Zona de Máquinas"
+    },
+    {
+        src: "/images/image4_Aire_Libre.jpeg",
+        alt: "Aire Libre",
+        titulo: "Aire Libre"
+    }
+];
+
+function LugaresInfo() {
+    return (
+      <div className="sites">
+        {lugares.map((lugar, idx) => (
+          <div className="card-lugar" key={idx}>
+            <img src={lugar.src} alt={lugar.alt}/>
+            <div className="titulo-lugar">{lugar.titulo}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default LugaresInfo;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,34 @@
-import Button from '../../components/Button';
-import { useNavigate } from 'react-router-dom';
+import Button from "../../components/Button";
+import LugaresInfo from "../../components/LugaresInfo";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    const navigate = useNavigate();
-    return (
-        <div className="home">
-            <header class = "title">
-                <div class = "logo">
-                    <img src = "/images/uis-logo.svg" alt = "logo de la UIS"/>
-                </div>
-                <div class = "title-content">
-                    <h1>UISport</h1>
-                    <p>¡Consulta nuestros espacios de gimnasio disponibles para tu próxima sesión!</p>
-                    <Button
-                        text="¡Reserva ahora!"
-                        onClick={() => navigate('/login')}
-                    />
-                </div>
-            </header>
-            <main></main>
-            <footer></footer>
+  const navigate = useNavigate();
+
+  return (
+    <div className="home">
+      <header className="title">
+        <div className="logo">
+          <img src="/images/uis-logo.svg" alt="logo de la UIS" />
         </div>
-    );
+        <div className="title-uis">
+          <h1>UISport</h1>
+          <p>
+            ¡Consulta nuestros espacios de gimnasio disponibles para tu próxima
+            sesión!
+          </p>
+        </div>
+      </header>
+      <main>
+
+        <Button text="¡Reserva ahora!" onClick={() => navigate("/login")} />
+        <h3 className="main-title">Más lugares donde puedes Entrenar en la UIS</h3>
+
+        <LugaresInfo />
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default Home; //default Home--> Este es el componente principal de este archivo Home
